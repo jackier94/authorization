0815da73d9a09ea5dbc4fb2d51b546c432ed6ac3
Jaquelin Rocha <jaquir16@gmail.com>
2020-10-01 21:07:29 -0700

    added loginBackend folder to project and copied build folder into it

---
+++ loginBackend/build/precache-manifest.0e6df6bc77e57ac04fd8af6d47083ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a07813e787a4f6ec5c2911008f27dd0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8766c65d45812ecdce26",
+    "url": "./static/css/main.41bac917.chunk.css"
+  },
+  {
+    "revision": "22d08900f6c43943b940",
+    "url": "./static/js/2.d44b7960.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.d44b7960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8766c65d45812ecdce26",
+    "url": "./static/js/main.92a31cba.chunk.js"
+  },
+  {
+    "revision": "c7488b0e57aa2f505f5e",
+    "url": "./static/js/runtime-main.800e563e.js"
+  }
+]);
